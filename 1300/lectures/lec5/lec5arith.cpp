708a2d40d4fb813038a1d584e5090a78c660eda9
C22Park <111820987+C22Park@users.noreply.github.com>
2022-08-31 16:07:10 -0600

    lec5

---
+++ 1300/lectures/lec5/lec5arith.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // declare an int
+
+    // declare another int
+
+    // divide one int by the other USING INTEGER DIVISION, print result
+
+    // divie one int by the other USING FLOATING POINT DIVISION, print result
+
+    // print out remainder of dividing one int by the other (hint: modulo operator)
+
+
+    return 0;
+}
---
+++ 1300/lectures/lec5/lec5input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // print prompt asking user for their favorite LETTER
+
+    // get the input from the user
+
+    // print out their favorite letter
+
+    // print out the INTEGER representation of their favorite letter 
+    // (remember our friend, the ASCII table?)
+    
+
+    return 0;
+}
