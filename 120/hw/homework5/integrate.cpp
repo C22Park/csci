c1ba5f2c301f3d4f9288db39867a4ff0cb4ccc57
C22Park <charlie22park@gmail.com>
2023-02-18 21:54:34 +0000

    cs120

---
+++ 120/hw/homework5/integrate.cpp
@@ -0,0 +1,38 @@
+/*
+
+*/
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+  //function y = x
+
+  int startingX = 0;
+  int stoppingX = 4;
+
+  double height = 0.0;
+  double width = 0.0;
+  double area = 0.0;
+  double stepSize = 0.0;
+  double sum = 0.0;
+  double Steps = 100000.0;
+
+  stepSize = stoppingX / Steps;
+
+  for(int i = 0; i < Steps; i++){
+  double s = startingX + i * stepSize;
+  double f = s + stepSize;
+
+  height = (f + s) / 2; // did a middle bound reimann sum versus a right or left
+  width = f - s;
+  area = height * width;
+  sum += area;
+  cout << "Step " << i+1 << " area " << area << endl;
+  }
+  cout << "Total area: " << sum << endl;
+
+
+
+}
--- 120/hw/homework5/textAdventure.cpp
+++ 120/hw/homework5/textAdventure.cpp
@@ -78,8 +78,11 @@ int questManager(string input, int num) {
     if (input == "add_gold") {
         gold += num;
     }
-    if (input == "gold") {
+    if (input == "print_gold") {
         cout << "Current Gold: " << gold << "\n";
+    }
+    if (input == "gold") {
+        return gold;
     }
     return 0;
 }
@@ -88,7 +91,7 @@ int start() {
     int input = 0;
     while (input < 1 || input > 2) {
         system("clear");
-        questManager("gold", 0);
+        questManager("print_gold", 0);
         if (questManager("forgot", 0)) {
             cout << "You wake up with a searing headache unsure of where you are. ";
             if (questManager("kick", 0)) {
@@ -122,7 +125,7 @@ int pond() {
     int input = 0;
     while (input < 1 || input > 2) {
         system("clear");
-        questManager("gold", 0);
+        questManager("print_gold", 0);
         if (questManager("kick", 0) == 0) {
             cout << "You fall into the water and are greeted by a fishman who tries to pull you deeper.\n"
                  << "What do you do?\n"
@@ -141,7 +144,7 @@ int pond() {
         }
         if (questManager("kick", 0)) {
             system("clear");
-            questManager("gold", 0);
+            questManager("print_gold", 0);
             cout << "The fishman is enraged and chants a spell before you get launched out the pond.\n"
                  << "What do you do?\n"
                  << "1) Flail your arms and scream\n"
@@ -162,7 +165,7 @@ int forest() {
     int input = 0;
     while (input < 1 || input > 3) {
         system("clear");
-        questManager("gold", 0);
+        questManager("print_gold", 0);
         cout << "You find a path that heads towards the mountains in one direction and a town in the other direction."
              << "What will you do?\n"
              << "1) Head for the hills\n"
@@ -188,7 +191,7 @@ int fishCity() {
     static bool first = true;
     while (input < 1 || input > 2) {
         system("clear");
-        questManager("gold", 0);
+        questManager("print_gold", 0);
         cout << "The fishman brought you to his underwater village where you can surprisingly breath.\n";
         if (first) {
             cout << "The village elder asks you to find a magic stone and bring it back for a reward.\n";
@@ -207,6 +210,7 @@ int fishCity() {
             }
             if (input == 1) {
                 system("clear");
+                questManager("print_gold", 0);
                 if (questManager("stone", 0)) {
                     cout << "The village elder thanks you and gives you 50 gold as a reward.\n"
                         << "Press enter to continue.\n";
@@ -243,7 +247,7 @@ int goblinCamp() {
     int input = 0;
     while (input != 5) {
         system("clear");
-        questManager("gold", 0);
+        questManager("print_gold", 0);
         cout << "You find an abandoned goblin camp at the base of a pecuiliar waterfall.\n"
              << "What do you do?\n";
         if (looted == false && questManager("quest", 0) == 0) {
@@ -253,6 +257,7 @@ int goblinCamp() {
             cin.ignore();
             if (input == 1) {
                 system("clear");
+                questManager("print_gold", 0);
                 cout << "You didn't find much of value except a chest full of 25 gold.\n"
                      << "Press enter to continue.\n";
                 looted = true;
@@ -273,6 +278,7 @@ int goblinCamp() {
             cin.ignore();
             if (input == 1) {
                 system("clear");
+                questManager("print_gold", 0);
                 cout << "You didn't find much of value except a chest full of 25 gold.\n"
                      << "Press enter to continue.\n";
                 looted = true;
@@ -280,11 +286,13 @@ int goblinCamp() {
                 cin.ignore();
             } else if (input == 2) {
                 system("clear");
+                questManager("print_gold", 0);
                 cout << "At the back of the waterfall you find a blue gem full of magic energy.\n"
                      << "Press enter to take it.\n";
                 questManager("change_stone", 0);
                 cin.ignore();
                 system("clear");
+                questManager("print_gold", 0);
                 cout << "The opening behind the waterfall begins to close.\n"
                      << "Press enter to continue.\n";
                 cin.ignore();
@@ -313,11 +321,13 @@ int goblinCamp() {
             cin.ignore();
             if (input == 1) {
                 system("clear");
+                questManager("print_gold", 0);
                 cout << "At the back of the waterfall you find a blue gem full of magic energy.\n"
                      << "Press enter to take it.\n";
                 questManager("change_stone", 0);
                 cin.ignore();
                 system("clear");
+                questManager("print_gold", 0);
                 cout << "The opening behind the waterfall begins to close.\n"
                      << "Press enter to continue.\n";
                 cin.ignore();
@@ -366,6 +376,7 @@ int town() {
     int input = 0;
     while (input < 1 || input > 3) {
         system("clear");
+        questManager("print_gold", 0);
         cout << "You enter the town of Longboum, there's a large tavern in front of you and behind it is an imposing marble tower.\n"
             << "What do you do?\n"
             << "1) Head to the tavern\n"
@@ -381,9 +392,115 @@ int town() {
             return 3;
         } else {
             cout << "Invalid input. Press enter.\n";
-                cin.ignore();
+            cin.ignore();
         }
     }
+}
+
+int tavern() {
+    static bool bet_taken = false;
+    int input = 0;
+    while (1) {
+        system("clear");
+        questManager("print_gold", 0);
+        cout << "You enter a dark tavern packed full of people. In the corner there's a group making card bets.\n"
+             << "What do you do?\n";
+        if (bet_taken == false) {
+            cout << "1) Drink\n"
+                 << "2) Make a bet\n"
+                 << "3) Head back to town\n";
+            cin >> input;
+            cin.ignore();
+            if (input == 1) {
+                system("clear");
+                questManager("print_gold", 0);
+                cout << "You begin to drink all night at the tavern. At some point you black out.\n"
+                     << "Press enter to continue.\n";
+                questManager("change_forgot", 0);
+                if (questManager("kick", 0)) {
+                    questManager("change_kick", 0);
+                }
+                cin.ignore();
+                return 1;
+            } else if (input == 2) {
+                system("clear");
+                questManager("print_gold", 0);
+                if (questManager("gold", 0) >= 10) {
+                    cout << "You put in 10 gold on a game of blackjack and win 35 gold in return.\n"
+                         << "Press enter to continue.\n";
+                    questManager("add_gold", 25);
+                    bet_taken = true;
+                    cin.ignore();
+                } else {
+                    cout << "You don't have enough gold to make a bet.\n"
+                         << "Press enter to continue.\n";
+                    cin.ignore();
+                }
+            } else if (input == 3) {
+                return 6;
+            } else {
+                cout << "Invalid input. Press enter.\n";
+                cin.ignore();
+            }
+        } else {
+            cout << "1) Drink\n"
+                 << "2) Head back to town\n";
+            cin >> input;
+            cin.ignore();
+            if (input == 1) {
+                system("clear");
+                questManager("print_gold", 0);
+                cout << "You begin to drink all night at the tavern. At some point you black out.\n"
+                     << "Press enter to continue.\n";
+                questManager("change_forgot", 0);
+                if (questManager("kick", 0)) {
+                    questManager("change_kick", 0);
+                }
+                cin.ignore();
+                return 1;
+            } else if (input == 2) {
+                return 6;
+            } else {
+                cout << "Invalid input. Press enter.\n";
+                cin.ignore();
+            }
+        }
+    }
+}
+
+int mageTower() {
+    int input;
+    bool run = true;
+    while (run) {
+        system ("clear");
+        questManager("print_gold", 0);
+        cout << "You enter the magic tower and an old man with a long white beard comes up to you.\n"
+             << "He tells you have a curse which sends you back in time whenever you drink.\n"
+             << "What do you do?\n"
+             << "1) Ask him to remove the curse\n"
+             << "2) Go back to town\n";
+        cin >> input;
+        cin.ignore();
+        if (input == 1) {
+            system("clear");
+            questManager("print_gold", 0);
+            if (questManager("gold", 0) >= 100) {
+                cout << "The wizard charges you 100 gold and promptly removes the curse. You are now free!\n"
+                     << "Thanks for playing. Press enter to quit.\n";
+                cin.ignore();
+                return 9;
+            } else {
+                cout << "The wizard wants to charge you 100 gold for the service. Come back when you get your money up.\n"
+                     << "Press enter to continue\n";
+                cin.ignore();
+            }
+        } else if (input == 2) {
+            return 6;
+        } else {
+            cout << "Invalid input. Press enter.\n";
+            cin.ignore();
+        }
+    }   
 }
 
 int main() {
@@ -409,14 +526,15 @@ int main() {
             case 6:
                 path = town();
                 break;
-            /*case 7:
+            case 7:
                 path = tavern();
                 break;
             case 8:
-                path = mageTower();
-            default:
+                mageTower();
+                break;
+            case 9:
                 run = false;
-                break;*/
+                break;
         }
     }
 }
